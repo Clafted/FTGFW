5b890485fd1a6867e4740d0954c5784d0915f60a
Noah Perez <noahm.p.in2017@gmail.com>
2023-12-05 14:39:16 -0500

    Startup

--- FTGFW/Main.cpp
+++ FTGFW/Main.cpp
@@ -2,12 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "Shader.hpp"
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
+
 int main()
 {
 	// Initial GLFW configuration
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	if (glfwInit() == GLFW_FALSE)
+	{
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
 	// Create a GLFW window
@@ -19,15 +27,54 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glViewport(0, 0, 800, 600);
+
+	// Set callbacks
+	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
 	// Initialize GLAD
-	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == NULL)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	Shader shader("vertex.vert", "fragment.frag");
+	glBindVertexArray(VAO);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Free resouces and terminate program
 	glfwTerminate();
 	return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height) 
+{
+	glViewport(0, 0, width, height);
 }
